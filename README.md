9113113957d659a007098ef5063a1f67f74ce70b
Alexander <alex.steve.elias@gmail.com>
2015-07-22 09:57:56 -0700

    Add Email Sending of POST/Updated routes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rust-iron-handlebars
+
+Quick server template using Rust and the Iron framework with Handlebars as the template engine.
+
+Has post and sends emails.
--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,12 @@ use mount::Mount;
 use handlebars_iron::{Template, HandlebarsEngine};
 use urlencoded::UrlEncodedBody;
 use rustc_serialize::json::{ToJson};
-// use rustc_serialize::json;
 
 use std::path::Path;
-// use std::io::Read;
 use std::collections::{BTreeMap};
-use std::process::Command;
 
-#[derive(RustcDecodable, RustcEncodable)]
-pub struct Info {
-    firstname: String
-}
-
+extern crate sendmail;
+use sendmail::email;
 
 fn main() {
 
@@ -52,38 +46,40 @@ fn main() {
         res.set_mut(Template::new("contact", data)).set_mut(status::Ok);
         Ok(res)
     };
-    fn submitted(req: &mut Request) -> IronResult<Response> {
+    fn confirmation(req: &mut Request) -> IronResult<Response> {
 
-        // Gets the Encoded URL from the POST, Check with match, and get user var and print
+        //Gets the Encoded URL from the POST, Check with match, and get user var and print
         let payload = req.get_ref::<UrlEncodedBody>();
         match payload {
             Ok(ref hashmap) => {println!("Parsed POST request body:\n {:?}", hashmap);},
             Err(ref e) => println!("{:?}", e)
         };
         let payload = payload.unwrap();
-        let payload = &payload["firstname"][0];
-        println!("{:?}", payload);
+        let firstname = &payload["firstname"][0];
 
-        // Pass post variable to comand and output
-        let output = Command::new("echo").arg(payload).output().unwrap_or_else(|e| {
-            panic!("failed to execute process: {}", e)
-        });
-        println!("status: {}", output.status);
-        println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-        println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+        //Setup and send email
+        email::create(
+            "to@example.com",
+            "from@example.com",
+            "Subject - Post Variables",
+            firstname,
+        );
+        email::send("alex.steven.elias@gmail.com");
 
+        //Setup page variables
         let mut res = Response::new();
         let mut data = BTreeMap::new();
-        data.insert("title".to_string(), "Submitted".to_json());
-        // Use same page i.e. (contact) or use new page i.e. (submit)
-        res.set_mut(Template::new("contact", data)).set_mut(status::Ok);
+        data.insert("title".to_string(), "Confirmation".to_json());
+
+        //Use same page i.e. (contact) or use new page i.e. (submit)
+        res.set_mut(Template::new("confirmation", data)).set_mut(status::Ok);
         Ok(res)
     };
     //Routes
 
 
     //Add routes to router
-    router.get("/", index).get("/contact", contact).post("/submitted", submitted);
+    router.get("/", index).get("/contact", contact).post("/confirmation", confirmation);
 
     //Chaing link the router
     let mut chain = Chain::new(router);
